ce77047b23e236206943c40387c99b08240e6a39
sudoweilder <robertec2007@gmail.com>
2025-02-20 16:03:44 -0800

    general file added, more detail added to game.java and snake.java, and more recent version of drawingpanel found.

--- Game.java
+++ Game.java
@@ -1,5 +1,17 @@
+import java.awt.*;
+
+
+
 public class Game{
-  public static void main(String[] args){
-    System.out.println();
-  }
+    public static final int panelWidth = 520; // current setup--could be changed by the user in the future. 
+    
+    public static void main(String[] args){
+	System.out.println();
+
+	//each block is 20x20 pixels as a result, this creates a 25X25 large grid(lines not added)
+	DrawingPanel panel = new DrawingPanel(panelWidth, panelWidth);
+	panel.setBackground(Color.BLACK);
+
+	Snake snake = new Snake(panel.getGraphics());
+    }
 }
--- Snake.java
+++ Snake.java
@@ -1,3 +1,28 @@
+import java.awt.Graphics.*;
+import java.awt.*;
+import java.util.ArrayList;
+
 public class Snake{
-    int[] heading = {0,0};
+    private int[] heading = {0,0};
+    private Graphics console;
+    private ArrayList<Point> body = new ArrayList<>();
+
+    public Snake(Graphics console){ //initializes the snake object
+	this.console = console; 
+	body.add(new Point(0,0));
+	//console.fillRect(20*
+    }
+    
+    
+    //500x500 center point
+    
+    public void grow(){
+    }
+
+    public void move(){
+    }
+    
+    public boolean isDead(){
+	return false;
+    }
 }
