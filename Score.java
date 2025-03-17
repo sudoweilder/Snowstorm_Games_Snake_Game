301877ca5758147bc3babeaea57796420e95e49c
DeathBySquid <jt.steger@icloud.com>
2025-03-17 16:15:57 -0700

    Fixed capitalization

--- Score.java
+++ Score.java
@@ -59,7 +59,7 @@ public class Score {
     }
 
     public static void displayScoreboard(Scanner console) throws FileNotFoundException, InterruptedException { // displays the scoreboard when called
-        Scanner display = new Scanner(new File("scoreboard.txt")); // this will take the sorted list
+        Scanner display = new Scanner(new File("Scoreboard.txt")); // this will take the sorted list
         int rank = 1; // initialize rank
         while (display.hasNextLine()) { // loops through each line in the file
             String line = display.nextLine();
